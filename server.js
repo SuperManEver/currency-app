ea743f4e1ae0222fbfb1b873a06b88669f0d9002
Nick Luparev <nikita.luparev@gmail.com>
2019-03-04 16:46:09 +0500

    changes for deploy

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+const staticDirPath = path.resolve(__dirname, 'build');
+
+// console.log(staticDirPath);
+
+// serve static assets normally
+// app.use(express.static('build'));
+
+app.use('/', express.static(staticDirPath));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port);
+console.log('server started on port ' + port);
