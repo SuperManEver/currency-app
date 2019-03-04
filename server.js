a7fc44d0260eefe6e9a2f998a874e6a4a184da9b
Nick Luparev <nikita.luparev@gmail.com>
2019-03-04 17:00:44 +0500

    attempt

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
@@ -17,6 +18,31 @@ app.use(express.static(staticDirPath));
 app.get('/', function(req, res) {
   // res.sendFile(path.resolve(path.join(__dirname, 'build', 'index.html')));
   res.send('hello world!');
+});
+
+app.listen(port);
+console.log('server started on port ' + port);
+
+*/
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+
+const staticDirPath = path.resolve(__dirname, 'build');
+
+// console.log(staticDirPath);
+
+// serve static assets normally
+// app.use(express.static('build'));
+
+app.use('/', express.static(staticDirPath));
+
+// handle every other route with index.html, which will contain
+// a script tag to your application's JavaScript file(s).
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
