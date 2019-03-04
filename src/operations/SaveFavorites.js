fbacf7a1e0f2dff4a88592f758ddd280b0413700
Nick Luparev <nikita.luparev@gmail.com>
2019-03-04 17:08:20 +0500

    remove debug console

--- src/operations/SaveFavorites.js
+++ src/operations/SaveFavorites.js
@@ -2,8 +2,6 @@ import { FAVORITES_LOCAL_STORAGE } from 'shared/constants';
 
 class SaveFavorites {
   run(store) {
-    console.log('save');
-
     localStorage.setItem(
       FAVORITES_LOCAL_STORAGE,
       JSON.stringify(store.favoritesCurrencies.slice()),
