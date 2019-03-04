97723b3ce5aa600d20e7b178597862da5a8ae040
Nick Luparev <nikita.luparev@gmail.com>
2019-03-04 16:54:51 +0500

    static server tweak

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const app = express();
 
-const staticDirPath = path.resolve(__dirname, 'build');
+const staticDirPath = path.resolve(path.join(__dirname, 'build'));
 
 // console.log(staticDirPath);
 
 // serve static assets normally
-// app.use(express.static('build'));
+app.use(express.static(staticDirPath));
 
-app.use('/', express.static(staticDirPath));
+// app.use('/', express.static(staticDirPath));
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function(req, res) {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+  res.sendFile(path.resolve(path.join(__dirname, 'build', 'index.html')));
 });
 
 app.listen(port);
